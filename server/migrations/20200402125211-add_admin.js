dc01b4af20024b5c2b3c2b04f4bf4c76499d130d
LMario <junior@degiant.com.br>
2020-04-03 18:05:26 -0300

    :card_file_box: Admin initial migration
    set the admin username as "admin" by default

--- server/migrations/20200402125211-add_admin.js
+++ server/migrations/20200402125211-add_admin.js
@@ -3,7 +3,7 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('TB_USERS', [{
-      username: 'Barionix',
+      username: 'admin',
       password: 'adminadmin',
       type: 'admin',
       createdAt: new Date(),
