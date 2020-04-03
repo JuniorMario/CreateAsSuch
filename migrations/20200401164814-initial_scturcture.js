da722b83624e73e40535af09bbc4d9ca5ca84317
LMario <junior@degiant.com.br>
2020-04-03 01:45:40 -0300

    :card_file_box: Initial migrations
    added posts to initial migrations

--- migrations/20200401164814-initial_scturcture.js
+++ migrations/20200401164814-initial_scturcture.js
@@ -64,8 +64,16 @@ module.exports = {
         defaultValue: Sequelize.NOW
       },
     })
+    await queryInterface.bulkInsert('TB_POSTS', [{
+      title: 'Lorem Ipsum',
+      subtitle: 'Memento Mori',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam hendrerit nisi sed sollicitudin pellentesque. Nunc posuere purus rhoncus pulvinar aliquam. Ut aliquet tristique nisl vitae volutpat.Nulla aliquet porttitor venenatis. Donec a dui et dui fringilla consectetur id nec massa. Aliquam erat volutpat. Sed ut dui ut lacus dictum fermentum vel tincidunt neque. Sed sed lacinia lectus.Duis sit amet sodales felis. Duis nunc eros, mattis at dui ac, convallis semper risus. In adipiscing ultrices tellus, in suscipit massa vehicula eu.',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }]);
     return Promise.resolve()
   },
+  
   
   down: async (queryInterface, Sequelize) => {
     await queryInterface.dropTable('TB_POSTS');
